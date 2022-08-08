93a4f5081492dd9fd7a3de8175e0075da794c032
senavarro <serginavarro.ro@gmail.com>
2022-08-08 16:16:43 +0100

    gifs added

--- dist/index.js
+++ dist/index.js
@@ -9002,11 +9002,20 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
+const fetch = __nccwpck_require__(467); 
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 async function run(){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
+
+    const randomGif = Math.round(Math.random() * 1000);
+    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomGif}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+
+    const response = await fetch(url);
+    const {results} = await response.json();
+    const gifUrl = results[0].media[0].tinygif.url;
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
@@ -9016,7 +9025,7 @@ async function run(){
     await octokit.rest.issues.createComment ({
         ...context.repo,
         issue_number: pull_request.number,
-        body: 'Thank you for submitting a pull request! We we will try to review this as soon as we can'
+        body: `Thank you for submitting a pull request! We we will try to review this as soon as we can. \n\n <img src="${gifUrl}" alt="thank you" />`
     });
 }
 
